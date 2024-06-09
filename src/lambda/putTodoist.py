1bce80a8ff44fa6c06b8668ca1af1e5827e24e69
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-09 17:29:11 -0400

    chore: Update AWS Lambda function configuration, Terraform configuration, and workflows

--- src/lambda/putTodoist.py
+++ src/lambda/putTodoist.py
@@ -1,14 +1,12 @@
 import requests
 import os
 
+
 def lambda_handler(event, context):
-    task = json.loads(event['body'])
-    api_token = os.getenv('TODOIST_API_TOKEN')
+    task = json.loads(event["body"])
+    api_token = os.getenv("TODOIST_API_TOKEN")
     headers = {"Authorization": f"Bearer {api_token}"}
-    
+
     response = requests.delete(f'https://api.todoist.com/rest/v1/tasks/{task["id"]}', headers=headers)
-    
-    return {
-        'statusCode': response.status_code,
-        'body': json.dumps({"message": "Task deleted"})
-    }
+
+    return {"statusCode": response.status_code, "body": json.dumps({"message": "Task deleted"})}
