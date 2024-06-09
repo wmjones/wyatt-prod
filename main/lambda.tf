2b4c117d471984a33a385fb87d1c9e54b0836eb1
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-09 17:30:36 -0400

    chore: Add variable for Todoist API key in Terraform configuration (#13)
    * chore: Add variable for Todoist API key in Terraform configuration
    
    * chore: Update AWS Lambda function configuration in main.tf
    
    * chore: Update AWS Lambda function configuration, Terraform configuration, and workflows

--- main/lambda.tf
+++ main/lambda.tf
@@ -1,15 +1,20 @@
-resource "aws_lambda_function" "todoist_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "todoist_lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda_function.handler"
-  runtime          = "python3.8"
-  source_code_hash = filebase64sha256("getTodoist.py.zip")
+variable "todoist_api_key" {
+    description = "API key for Todoist"
+    default     = ""
+}
 
-  environment {
-    variables = {
-      TODOIST_API_KEY = "9593c48930e891084fde61fa56a7a5c59336535e"
-      S3_BUCKET_NAME  = aws_s3_bucket.data.bucket
+resource "aws_lambda_function" "todoist_lambda" {
+    filename         = "lambda_function.zip"
+    function_name    = "todoist_lambda"
+    role             = aws_iam_role.lambda_role.arn
+    handler          = "lambda_function.handler"
+    runtime          = "python3.8"
+    source_code_hash = filebase64sha256("getTodoist.py.zip")
+
+    environment {
+        variables = {
+            TODOIST_API_KEY = var.todoist_api_key
+            S3_BUCKET_NAME  = aws_s3_bucket.data.bucket
+        }
     }
-  }
 }
--- src/lambda/getTodoist.py
+++ src/lambda/getTodoist.py
@@ -3,26 +3,19 @@ import boto3
 import requests
 import os
 
+
 def lambda_handler(event, context):
-    todoist_api_key = os.environ['TODOIST_API_KEY']
-    s3_bucket_name = os.environ['S3_BUCKET_NAME']
+    todoist_api_key = os.environ["TODOIST_API_KEY"]
+    s3_bucket_name = os.environ["S3_BUCKET_NAME"]
 
-    headers = {
-        'Authorization': f'Bearer {todoist_api_key}'
-    }
+    headers = {"Authorization": f"Bearer {todoist_api_key}"}
 
-    response = requests.get('https://api.todoist.com/rest/v1/tasks?filter=(status=1)', headers=headers)
+    response = requests.get("https://api.todoist.com/rest/v1/tasks?filter=(status=1)", headers=headers)
     data = response.json()
 
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     s3.put_object(
-        Bucket=s3_bucket_name,
-        Key='data/incomplete_tasks.json',
-        Body=json.dumps(data),
-        ContentType='application/json'
+        Bucket=s3_bucket_name, Key="data/incomplete_tasks.json", Body=json.dumps(data), ContentType="application/json"
     )
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Data saved to S3')
-    }
+    return {"statusCode": 200, "body": json.dumps("Data saved to S3")}
