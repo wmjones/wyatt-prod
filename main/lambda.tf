c626eefde8a764125dff1a1564fb94493506a1f8
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-09 17:28:09 -0400

    chore: Update AWS Lambda function configuration in main.tf

--- main/lambda.tf
+++ main/lambda.tf
@@ -1,19 +1,20 @@
 variable "todoist_api_key" {
-  description = "API key for Todoist"
+    description = "API key for Todoist"
+    default     = ""
 }
 
 resource "aws_lambda_function" "todoist_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "todoist_lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda_function.handler"
-  runtime          = "python3.8"
-  source_code_hash = filebase64sha256("getTodoist.py.zip")
+    filename         = "lambda_function.zip"
+    function_name    = "todoist_lambda"
+    role             = aws_iam_role.lambda_role.arn
+    handler          = "lambda_function.handler"
+    runtime          = "python3.8"
+    source_code_hash = filebase64sha256("getTodoist.py.zip")
 
-  environment {
-    variables = {
-      TODOIST_API_KEY = var.todoist_api_key
-      S3_BUCKET_NAME  = aws_s3_bucket.data.bucket
+    environment {
+        variables = {
+            TODOIST_API_KEY = var.todoist_api_key
+            S3_BUCKET_NAME  = aws_s3_bucket.data.bucket
+        }
     }
-  }
 }
