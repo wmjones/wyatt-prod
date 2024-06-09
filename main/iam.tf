af81459575b589931dc85755e3dd6206896adf9f
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-09 16:31:14 -0400

    chore: Refactor Terraform code for step function and IAM roles

--- main/iam.tf
+++ main/iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_exec_role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com",
       },
@@ -14,9 +14,9 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "lambda_policy"
-  role   = aws_iam_role.lambda_role.id
-  
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_role.id
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -29,8 +29,8 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Resource = "${aws_s3_bucket.data.arn}/*",
       },
       {
-        Action = "logs:*",
-        Effect = "Allow",
+        Action   = "logs:*",
+        Effect   = "Allow",
         Resource = "*"
       }
     ],
--- main/step_func.tf
+++ main/step_func.tf
@@ -1,15 +1,15 @@
 resource "aws_sfn_state_machine" "step_function" {
   name     = "TodoistStepFunction"
   role_arn = aws_iam_role.lambda_role.arn
-  
+
   definition = jsonencode({
-    Comment: "A description of my state machine",
-    StartAt: "GetIncompleteTasks",
-    States: {
-      GetIncompleteTasks: {
-        Type: "Task",
-        Resource: aws_lambda_function.todoist_lambda.arn,
-        End: true
+    Comment : "A description of my state machine",
+    StartAt : "GetIncompleteTasks",
+    States : {
+      GetIncompleteTasks : {
+        Type : "Task",
+        Resource : aws_lambda_function.todoist_lambda.arn,
+        End : true
       }
     }
   })
