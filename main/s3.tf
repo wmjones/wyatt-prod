2fb25a839bbf65c0a4c2832cb3005ea62a0f4e8d
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-09 18:06:02 -0400

    chore: Update AWS Lambda function filename in Terraform configuration

--- main/s3.tf
+++ main/s3.tf
@@ -1,32 +1,3 @@
-locals {
-  bucket_name = "wyatt-lake"
-}
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = local.bucket_name
-  acl    = "private"
-
-  force_destroy = true
-
-  versioning = {
-    enabled = true
-  }
-
-  lifecycle_rule = [{
-    id      = "step_function_bucket"
-    enabled = true
-    prefix  = "step_function_bucket/"
-
-    transition = [{
-      days          = 30
-      storage_class = "INTELLIGENT_TIERING"
-    }]
-    }
-  ]
-}
-
 resource "aws_s3_bucket" "step_function_bucket" {
-  bucket = local.bucket_name
+  bucket = "step-function-bucket"
 }
