5c543b76f4e4f6a1602502dd3f1d2497fbbf3057
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-10 14:37:41 -0400

    Dev (#31)
    * chore: Add variable for Todoist API key in Terraform configuration
    
    * chore: Update AWS Lambda function configuration in main.tf
    
    * chore: Update AWS Lambda function configuration, Terraform configuration, and workflows
    
    * update tf workspace name
    
    * chore: Update S3 bucket resource names in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update AWS Lambda function filename in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * rename bucket
    
    * rename bucket
    
    * update iam role
    
    * update iam role
    
    * lambda handler
    
    * chore: Update AWS Lambda function filename and source code hash
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * update handler path
    
    * chore: Update AWS Lambda function handler path
    
    * chore: Update AWS Lambda function handler path
    
    * chore: Update .gitignore to exclude deploy_folder and add getTodoist.py
    
    * refactor: Strip rich text from task content before saving to S3

--- src/lambda/getTodoist.py
+++ src/lambda/getTodoist.py
@@ -2,8 +2,10 @@ import json
 import os
 from todoist_api_python.api import TodoistAPI
 import boto3
+import re
 
 from dataclasses import dataclass, asdict
+from typing import List
 
 
 @dataclass
@@ -28,15 +30,24 @@ class Task:
     duration: str
     sync_id: str
 
+def strip_rich_text(text: str) -> str:
+    emoji_pattern = re.compile("["
+    u"\U0001F600-\U0001F64F"  # emoticons
+    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    u"\U00010000-\U0010ffff"
+    "]+", flags=re.UNICODE)
+    clean_text = emoji_pattern.sub(r'', text)
+    return clean_text
 
-def task_to_dict(task):
+def task_to_dict(task: Task) -> dict:
+    task.content = strip_rich_text(task.content)
     return asdict(task)
 
-
-def task_to_json(task):
-    task_dict = task_to_dict(task)
-    return json.dumps(task_dict, indent=4)
-
+def tasks_to_json(tasks: List[Task]):
+    tasks_dict = [task_to_dict(task) for task in tasks]
+    return json.dumps(tasks_dict, indent=4), tasks
 
 def lambda_handler(event, context):
     # todoist_api_key = os.environ["TODOIST_API_KEY"]
@@ -46,19 +57,25 @@ def lambda_handler(event, context):
 
     try:
         tasks = api.get_tasks()
-        parsed_tasks = [task_to_json(tasks[i]) for i in range(len(tasks))]
+        json_str, tasks_instances = tasks_to_json(tasks)
+        print(json_str)
+        print(tasks_instances)
+        # parsed_tasks = [task_to_json(tasks[i]) for i in range(len(tasks))]
 
         s3 = boto3.client("s3")
         s3.put_object(
             Bucket=s3_bucket_name,
             Key="data/incomplete_tasks.json",
-            Body=json.dumps(parsed_tasks),
-            ContentType="application/json",
+            Body=json_str
         )
     except Exception as error:
         print(error)
 
     return {"statusCode": 200, "body": json.dumps("Data saved to S3")}
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
 
 
 if __name__ == "__main__":
