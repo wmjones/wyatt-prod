a951ecbd0fc697397a6b97c4fabbb553026a94cb
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-10 08:26:19 -0400

    chore: Update .gitignore to exclude deploy_folder and add getTodoist.py

--- main/lambda.tf
+++ main/lambda.tf
@@ -10,12 +10,12 @@ variable "TODOIST_API_TOKEN" {
 # }
 
 resource "aws_lambda_function" "todoist_lambda" {
-  filename         = "deployment_pkg.zip"
+  filename         = "deployment_package.zip"
   function_name    = "todoist_lambda"
   role             = aws_iam_role.lambda_role.arn
   handler          = "getTodoist.lambda_handler" # Update the handler to the correct module and function name
   runtime          = "python3.8"
-  source_code_hash = filebase64sha256("deployment_pkg.zip")
+  source_code_hash = filebase64sha256("deployment_package.zip")
   environment {
     variables = {
       TODOIST_API_KEY = var.TODOIST_API_TOKEN
