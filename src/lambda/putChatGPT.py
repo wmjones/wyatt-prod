bf3cf0e72772918af7541ba0690748538517d57e
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-11 10:43:33 -0400

    chore: Update lambda timeout to 15 seconds (#50)

--- src/lambda/putChatGPT.py
+++ src/lambda/putChatGPT.py
@@ -6,6 +6,7 @@ import boto3
 
 def lambda_handler(event, context):
     s3_bucket_name = os.environ["S3_BUCKET_NAME"]
+    open_ai_key = os.environ["OPENAI_API_KEY"]
     # task = json.loads(event["body"])
     # Read from S3 incomplete_tasks.json and get the content field of each task
     s3 = boto3.client("s3")
@@ -16,19 +17,20 @@ def lambda_handler(event, context):
     tasks = [task["content"] for task in data]  # Extract the content field of each task
     print(f"tasks[0]: {tasks[0]}")
 
-    print(f"api_key: {os.environ['OPENAI_API_KEY']}")
     client = OpenAI(
         # This is the default and can be omitted
-        api_key=os.environ["OPENAI_API_KEY"],
+        api_key=open_ai_key,
     )
     print(f"client: {client}")
 
     with open("biftu.txt", "r") as file:
         biftu_system_prompt = file.read()
+    print(f"biftu_system_prompt: {biftu_system_prompt}")
 
     completions = []
 
     for task in tasks:
+        print(f"\ntask: {task}")
         chat_completion = client.chat.completions.create(
             messages=[
                 {
