7ab9521278d86e02d87c86ee1e41821b651b9650
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-10 21:15:27 -0400

    Dev (#35)
    * chore: Add variable for Todoist API key in Terraform configuration
    
    * chore: Update AWS Lambda function configuration in main.tf
    
    * chore: Update AWS Lambda function configuration, Terraform configuration, and workflows
    
    * update tf workspace name
    
    * chore: Update S3 bucket resource names in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update AWS Lambda function filename in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * chore: Update S3 bucket resource name in Terraform configuration
    
    * rename bucket
    
    * rename bucket
    
    * update iam role
    
    * update iam role
    
    * lambda handler
    
    * chore: Update AWS Lambda function filename and source code hash
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * chore: Update pre-commit hooks for black and terraform formatting
    
    * update handler path
    
    * chore: Update AWS Lambda function handler path
    
    * chore: Update AWS Lambda function handler path
    
    * chore: Update .gitignore to exclude deploy_folder and add getTodoist.py
    
    * refactor: Strip rich text from task content before saving to S3
    
    * merge in origin main
    
    * chore: Update function name in main.tf for AWS Lambda
    
    * chore: Update AWS Step Functions definition in main/step_func.tf

--- main/step_func.tf
+++ main/step_func.tf
@@ -1,16 +1,28 @@
 resource "aws_sfn_state_machine" "step_function" {
-  name     = "TodoistStepFunction"
-  role_arn = aws_iam_role.lambda_role.arn
+    name     = "TodoistStepFunction"
+    role_arn = aws_iam_role.lambda_role.arn
 
-  definition = jsonencode({
-    Comment : "A description of my state machine",
-    StartAt : "GetIncompleteTasks",
-    States : {
-      GetIncompleteTasks : {
-        Type : "Task",
-        Resource : aws_lambda_function.todoist_lambda.arn,
-        End : true
-      }
-    }
-  })
+    definition = jsonencode({
+        Comment : "A description of my state machine",
+        StartAt : "GetIncompleteTasks",
+        States : {
+            GetIncompleteTasks : {
+                Type : "Task",
+                Resource : aws_lambda_function.todoist_lambda.arn,
+                End : false,
+                Next : "PutChatGPT"
+            },
+            PutChatGPT : {
+                Type : "Task",
+                Resource : aws_lambda_function.putChatGPT_lambda.arn,
+                End : false,
+                Next : "PutNotion"
+            },
+            PutNotion : {
+                Type : "Task",
+                Resource : aws_lambda_function.putNotion_lambda.arn,
+                End : true
+            }
+        }
+    })
 }
