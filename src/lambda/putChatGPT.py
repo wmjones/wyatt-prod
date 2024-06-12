9c8b6c6c20ffaf874ed550e6d2aaeecfd2a448db
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-11 20:47:40 -0400

    Update putChatGPT.py to handle exceptions and include detailed Kanban user story prompt (#60)

--- src/lambda/putChatGPT.py
+++ src/lambda/putChatGPT.py
@@ -2,9 +2,11 @@ from openai import OpenAI
 import json
 import os
 import boto3
+from botocore.exceptions import ClientError
 
 
 def get_secret(secret_name, region_name):
+    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -16,19 +18,18 @@ def get_secret(secret_name, region_name):
             SecretId=secret_name
         )
     except ClientError as e:
-        raise Exception("Couldn't retrieve the secret") from e
-    else:
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-        
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
     return secret
 
 
 def lambda_handler(event, context):
     s3_bucket_name = os.environ["S3_BUCKET_NAME"]
-    open_ai_key = get_secret("OPEN_AI_KEY", "us-east-2")
+    secret = get_secret("open_ai_key", "us-east-2")
+    secret_dict = json.loads(secret)
     # task = json.loads(event["body"])
     # Read from S3 incomplete_tasks.json and get the content field of each task
     s3 = boto3.client("s3")
@@ -41,13 +42,13 @@ def lambda_handler(event, context):
 
     client = OpenAI(
         # This is the default and can be omitted
-        api_key=open_ai_key,
+        api_key=secret_dict['OPEN_AI_KEY'],
     )
     print(f"client: {client}")
 
     with open("biftu.txt", "r") as file:
         biftu_system_prompt = file.read()
-    print(f"biftu_system_prompt: {biftu_system_prompt}")
+    # print(f"biftu_system_prompt: {biftu_system_prompt}")
 
     completions = []
 
@@ -65,12 +66,11 @@ def lambda_handler(event, context):
                         "content": f"Create a detailed, actionable Kanban user story using this short prompt.\n{task}",
                     },
                 ],
-                model="gpt-3.5-turbo",
-                stream=True
+                model="gpt-3.5-turbo"
             )
 
             completions.append(
-                chat_completion.choices[0].message
+                chat_completion.choices[0].message.content
             )  # Append the content of the chat completion to the completions list
             break
     except Exception as error:
@@ -79,3 +79,7 @@ def lambda_handler(event, context):
 
     print(completions)
     return {"statusCode": 200, "body": json.dumps(completions)}  # Serialize completions list to JSON
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
