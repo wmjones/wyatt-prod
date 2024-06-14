8afd2bc8f0f3f67a624c8979619a29a0d4abc6a4
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-14 17:28:43 -0400

    feat: Close task using TodoistAPI in putTodoist lambda (#86)

--- src/lambda/putTodoist.py
+++ src/lambda/putTodoist.py
@@ -8,13 +8,14 @@ def lambda_handler(event, context):
     secret = get_secret("todoist_key", "us-east-2")
     secret_dict = json.loads(secret)
     todoist_api_key = secret_dict["TODOIST_API_KEY"]
-    headers = {"Authorization": f"Bearer {todoist_api_key}"}
+    api = TodoistAPI(todoist_api_key)
 
     for json_task in json.loads(event["body"]):
         try:
             print(f"\njson_task: {json_task}")
             task = SuperTask(**json_task)
-            response = requests.delete(f'https://api.todoist.com/rest/v1/tasks/{task.id}', headers=headers)
+            is_success = api.close_task(task_id=task.id)
+            print(is_success)
         except Exception as error:
             return {"statusCode": 500, "body": json.dumps({"error": str(error)})}
     return {"statusCode": response.status_code, "body": json.dumps({"message": "Task deleted"})}
