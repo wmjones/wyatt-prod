07b1d42dda6c3e8e79921d0d86f5da7479b4fb9f
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-15 12:08:47 -0400

    chore: Update IAM role for events.amazonaws.com service

--- main/iam.tf
+++ main/iam.tf
@@ -70,48 +70,48 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-resource "aws_iam_role" "sfn_role" {
-  name = "sfn_role"
+resource "aws_iam_role" "scheduler" {
+  name = "scheduler-role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
-          Service = "states.amazonaws.com"
+          Service = ["scheduler.amazonaws.com"]
         }
-      },
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy" "sfn_policy" {
-  name = "sfn_policy"
-  role = aws_iam_role.sfn_role.id
+resource "aws_iam_role_policy_attachment" "scheduler" {
+  policy_arn = aws_iam_policy.scheduler.arn
+  role       = aws_iam_role.scheduler.name
+}
+
+resource "aws_iam_policy" "scheduler" {
+  name = "scheduler-policy"
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
+        # allow scheduler to execute the task
+        Effect = "Allow",
         Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect   = "Allow",
-        Resource = "*"
+          "ecs:RunTask"
+        ]
+        # trim :<revision> from arn, to point at the whole task definition and not just one revision
+        Resource = [trimsuffix(aws_ecs_task_definition.task.arn, ":${aws_ecs_task_definition.task.revision}")]
       },
-      {
-        Action   = "logs:CreateLogGroup",
-        Effect   = "Allow",
-        Resource = "arn:aws:logs:*:*:*"
-      }
+      { # allow scheduler to set the IAM roles of your task
+        Effect = "Allow",
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = [aws_iam_role.task.arn, aws_iam_role.execution.arn]
+      },
     ]
   })
 }
