02e60d88aa4befee6bf95e99dc0eef9511701182
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-16 14:10:39 -0400

    chore: Update project IDs in putChatGPT.py and utils.py (#110)

--- src/lambda/putChatGPT.py
+++ src/lambda/putChatGPT.py
@@ -7,6 +7,13 @@ def lambda_handler(event, context):
     print(f"event: {event}")
     secret = get_secret("open_ai_key", "us-east-2")
     secret_dict = json.loads(secret)
+    with open("src/lambda/config.json", "r") as file:
+        config = json.load(file)
+    n_project_ids = len(config["projects"])
+    project_id_list = [
+        config['projects'][i]['project_id'] 
+        for i in range(n_project_ids) 
+    ]
 
     client = OpenAI(
         # This is the default and can be omitted
@@ -20,11 +27,10 @@ def lambda_handler(event, context):
             task = SuperTask(**json_task)
             print(f"\njson_task: {json_task}")
             print(f"\ntask: {task}")
-            if task.project_id == "2334044356":  # Home
-                list_task_dict.append(task)            
-            elif task.project_id == "2334637119":  # Weight
-                list_task_dict.append(task)
-            elif task.project_id == "2334044360":  # Work
+            if int(task.project_id) in project_id_list:
+                # access config.json to get passChatGPT for this task.project_id
+                task.passChatGPT = config['projects'][project_id_list.index(int(task.project_id))]['passChatGPT']
+                task.name = config['projects'][project_id_list.index(int(task.project_id))]['name']
                 if task.section_id == "158311513":  # Biftu
                     with open("biftu.txt", "r") as file:
                         system_prompt = file.read()
@@ -58,7 +64,7 @@ def lambda_handler(event, context):
 if __name__ == "__main__":
     event = {
         "statusCode": 200,
-        "body": '[\n    {\n        "assignee_id": null,\n        "assigner_id": null,\n        "comment_count": 0,\n        "is_completed": false,\n        "content": "add new todoist pipeline for weight",\n        "created_at": "2024-06-12T11:10:34.825542Z",\n        "creator_id": "49425011",\n        "description": "",\n        "due": null,\n        "id": "8110672962",\n        "labels": [],\n        "order": 1,\n        "parent_id": null,\n        "priority": 1,\n        "project_id": "2334637095",\n        "section_id": null,\n        "url": "https://todoist.com/app/task/8110672962",\n        "duration": null,\n        "sync_id": null\n    }\n]',
+        "body": '[\n    {\n        "assignee_id": null,\n        "assigner_id": null,\n        "comment_count": 0,\n        "is_completed": false,\n        "content": "add new todoist pipeline for weight",\n        "created_at": "2024-06-12T11:10:34.825542Z",\n        "creator_id": "49425011",\n        "description": "",\n        "due": null,\n        "id": "8110672962",\n        "labels": [],\n        "order": 1,\n        "parent_id": null,\n        "priority": 1,\n        "project_id": "2334044360",\n        "section_id": "158311513",\n        "url": "https://todoist.com/app/task/8110672962",\n        "duration": null,\n        "sync_id": null\n    }\n]',
     }
     out = lambda_handler(event, None)
     print(out)
