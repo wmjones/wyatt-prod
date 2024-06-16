534added76850f8e4f8526ee55ba9785c888decf
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-16 19:09:42 -0400

    chore: Update lambda function handlers (#113)
    ```

--- main/lambda.tf
+++ main/lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "todoist_lambda" {
   filename         = data.local_file.lambda_zip.filename
   function_name    = "todoist_lambda"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "getTodoist.lambda_handler" # Update the handler to the correct module and function name
+  handler          = "getTodoist.lambda_handler"
   runtime          = "python3.12"
   source_code_hash = filebase64sha256(data.local_file.lambda_zip.filename)
   environment {
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "chatgpt_lambda" {
   filename         = data.local_file.lambda_zip.filename
   function_name    = "ChatGPT_lambda"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "putChatGPT.lambda_handler" # Update the handler to the correct module and function name
+  handler          = "putChatGPT.lambda_handler"
   runtime          = "python3.12"
   timeout          = 15
   source_code_hash = filebase64sha256(data.local_file.lambda_zip.filename)
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "notion_lambda" {
   filename         = data.local_file.lambda_zip.filename
   function_name    = "notion_lambda"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "putNotion.lambda_handler" # Update the handler to the correct module and function name
+  handler          = "putNotion.lambda_handler"
   runtime          = "python3.12"
   timeout          = 10
   source_code_hash = filebase64sha256(data.local_file.lambda_zip.filename)
@@ -46,13 +46,13 @@ resource "aws_lambda_function" "notion_lambda" {
   }
 }
 
-// add a lambda for putTodoist
 resource "aws_lambda_function" "put_todoist_lambda" {
   filename         = data.local_file.lambda_zip.filename
   function_name    = "put_todoist_lambda"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "putTodoist.lambda_handler" # Update the handler to the correct module and function name
+  handler          = "putTodoist.lambda_handler"
   runtime          = "python3.12"
+  timeout          = 10
   source_code_hash = filebase64sha256(data.local_file.lambda_zip.filename)
   environment {
     variables = {
