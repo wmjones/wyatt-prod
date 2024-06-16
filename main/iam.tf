d3b22b86f7d530e528e9971b15b0bdf98fd69035
Wyatt Jones <wyatt.jones6@cfacorp.com>
2024-06-15 22:55:23 -0400

    fix trust relationship

--- main/iam.tf
+++ main/iam.tf
@@ -3,13 +3,21 @@ resource "aws_iam_role" "lambda_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com",
+        },
       },
-      },
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.sfn_role.arn
+        }
+      },      
       {
         Action = "sts:AssumeRole",
         Effect = "Allow",
